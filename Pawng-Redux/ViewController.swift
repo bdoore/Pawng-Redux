2f17db9bba0de244bd2176464d0ccb061d4d5b9c
Brian Doore <doore.b@gmail.com>
2014-12-08 09:27:57 -0600

    Initial Commit

---
+++ Pawng-Redux/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pawng-Redux
+//
+//  Created by Brian Doore on 12/8/14.
+//  Copyright (c) 2014 Brian Doore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
